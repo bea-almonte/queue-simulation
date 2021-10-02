aea624e7bdc18b904d00a5033e7e7f1d90c61ffb
Bea Almonte <laa44@students.uwf.edu>
2021-10-02 13:46:07 -0500

    Added test for random interval.

--- final-simulation.cpp
+++ final-simulation.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <time.h>
 #include "customer.hpp"
 #include "queue.hpp"
+#include "heap.hpp"
 
 int main() {
     Customer heap[5];
+    Heap customers;
     Queue line;
+    srand(time(NULL));
 
     for (int i = 0; i < 5; i++) {
         heap[i].arrivalTime = i;
@@ -15,5 +19,9 @@ int main() {
         std::cout << line.Dequeue()->arrivalTime << std::endl;
     }
 
+    for (int i = 0; i < 20; i++) {
+        std::cout << customers.GetNextRandomInterval() << " ";
+        
+    }
     return 0;
 }
