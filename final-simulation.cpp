24b676a4099f3581bdef4cb883e070a49e3e6cc9
Bea Almonte <laa44@students.uwf.edu>
2021-10-02 15:19:35 -0500

    Testing Percolate down.

--- final-simulation.cpp
+++ final-simulation.cpp
@@ -11,9 +11,21 @@ int main() {
     srand(time(NULL));
 
     customers.BuildHeap();
-    test.arrivalTime = 2.5;
+    test.arrivalTime = 1.5;
+    for (int i = 1; i < 11; i++) {
+        std::cout << customers.events[i].arrivalTime << " ";
+    }
     customers.PercolateUp(test);
+    std::cout << std::endl;
     for (int i = 1; i < 12; i++) {
+        std::cout << customers.events[i].arrivalTime << " ";
+    }
+
+    std::cout << std::endl;
+    customers.DeleteMin();
+    customers.DeleteMin();
+    customers.DeleteMin();
+    for (int i = 1; i < 9; i++) {
         std::cout << customers.events[i].arrivalTime << " ";
     }
     return 0;
